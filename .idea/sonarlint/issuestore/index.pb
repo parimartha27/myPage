5698ec6673ddb17060cb05adea32ab264fdd355e
parimartha27 <putu.parimartha@binus.ac.id>
2023-10-22 15:36:29 +0700

    initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+‚
+RlearnSpringBoot/src/main/java/com/learn/springboot/LearnSpringBootApplication.java,9\8\98ca0b31bb9d6a71955e642eb3f7266f5931dc73
---
+++ learnSpringBoot/src/main/java/com/learn/springboot/entity/User.java
@@ -0,0 +1,21 @@
+package com.learn.springboot.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "User")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+}
